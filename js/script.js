53faa32463f0f0c0ee97b79d7bb5b9baf1e7fede
kevensouzz <kevenssouza1@gmail.com>
2022-08-29 20:47:51 -0300

    Update Files

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function insert(num) {
+    var number = document.getElementById('result').innerHTML
+    document.getElementById('result').innerHTML = number + num
+}
+
+function clean() {
+    document.getElementById('result').innerHTML = ''
+}
+
+function back() {
+    var result = document.getElementById('result').innerHTML
+    document.getElementById('result').innerHTML = result.substring(0, result.length - 1)
+}
+
+function calcular() {
+    var result = document.getElementById('result').innerHTML
+
+    if(result) {
+        document.getElementById('result').innerHTML = eval(result)
+    }
+}
